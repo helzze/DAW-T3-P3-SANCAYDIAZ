0fe4348846b36c64df6d36678de352bdadf072ab
helzze <sebastiansancaowo@gmail.com>
2024-10-26 00:12:32 -0500

    pregunta 3

---
+++ src/main/java/t3/sancaydiaz/p3/t3_p3_sancaydiaz/T3P3SancaydiazApplication.java
@@ -0,0 +1,13 @@
+package t3.sancaydiaz.p3.t3_p3_sancaydiaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3P3SancaydiazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T3P3SancaydiazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/t3/sancaydiaz/p3/t3_p3_sancaydiaz/T3P3SancaydiazApplicationTests.java
@@ -0,0 +1,13 @@
+package t3.sancaydiaz.p3.t3_p3_sancaydiaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T3P3SancaydiazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
